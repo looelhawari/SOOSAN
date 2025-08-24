881f16fd0576e8481d840c7e0b7292bdaf89f740
kareemh122 <kareemhesham105@gmail.com>
2025-08-24 23:34:26 +0300

    currency translation

--- resources/lang/en/sold-products.php
+++ resources/lang/en/sold-products.php
@@ -25,6 +25,7 @@ return [
     'monthly_sales' => 'Monthly Sales',
     'yearly_sales' => 'Yearly Sales',
     'average_sale_value' => 'Average Sale Value',
+    'currency' => 'EGP',
 
     // Actions
     'add_new_sale' => 'Add New Sale',
